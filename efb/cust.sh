b1fa7bcae1085f4c7a997235ae942dcda12dd4ee
lowking <lowking@sina.cn>
2021-12-31 21:19:02 +0800

    feat(efb): init

---
+++ efb/cust.sh
@@ -0,0 +1,6 @@
+# qq opq 所需依赖
+apk add --no-cache gcc \
+musl-dev
+pip3 install -U wheel
+pip3 install -U git+https://github.com/milkice233/efb-qq-slave
+pip3 install git+https://github.com/milkice233/efb-qq-plugin-iot
